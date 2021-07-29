7de29aca4f001242306598a3d53eec93785c6c10
ohRaindrop <soldi.linda@gmail.com>
2021-07-29 18:51:22 +0200

    aesthetic done. needs to be copied + js

--- main.js
+++ main.js
@@ -1,20 +1,36 @@
 // CODE FOR TABS
-const tabcontent = document.querySelectorAll(".tabcontent");
-for (let tabc in tabcontent) {
-    tabc.style.display = "none";
-}
 
-let tablinks = document.querySelectorAll(".tablinks");
-for (let tabl in tablinks) {
-    tabl.replace(" active-tab", "");
-}
+// const mainTab = document.querySelector(".tablinks.main-tab");
+// const cjTab = document.querySelector(".tablinks.c-j-tab");
+// const iiTab = document.querySelector(".tablinks.i-i-tab");
+// const tvTab = document.querySelector(".tablinks.t-v-tab");
 
-// IDEA: take selected element, add active and displat content
-document.getElementById(cityName).style.display = "block";
-//     evt.currentTarget.className += " active";
+// mainTab.addEventListener('click', openTab("#Main"));
+// cjTab.addEventListener('click', openTab("#China-Japan"));
+// iiTab.addEventListener('click', openTab("#India-Indonesia"));
+// tvTab.addEventListener('click', openTab("#Thai-Vietnam"));
+
+
+// function openTab(kindOfFood) {
+//     console.log(kindOfFood);
+//     let tabcontent = document.querySelectorAll(".tabcontent");
+//     for (let i = 0; i < tabcontent.length; i++) {
+//         tabcontent[i].style.display = "none";
+//         console.log(tabcontent);
+//     }
+
+//     let tablinks = document.querySelectorAll(".tablinks");
+//     for (let i = 0; i < tablinks.length; i++) {
+//         tablinks[i].className = tablinks[i].className.replace(" active", "");
+//         console.log(tablinks);
+//     }
+//     document.querySelector(kindOfFood).style.display = "block";
+//     document.querySelector(kindOfFood).className += " active";
+//     console.log(kindOfFood);
+// }
 
 // CODE FOR EVENTS
 // IDEA: when you click on a "reserve table" button in event section, you go to the form and date will be automatically filled
-const reserveForm = document.querySelector("#reservation-form");
-const mainEvent = document.querySelector();
+// const reserveForm = document.querySelector("#reservation-form");
+// const mainEvent = document.querySelector();
 
