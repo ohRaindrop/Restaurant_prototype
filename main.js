fef83d90c543156733f063b3dc62dea6e10d3076
ohRaindrop <soldi.linda@gmail.com>
2021-08-01 17:37:15 +0200

    finished. php need but can be at once

--- main.js
+++ main.js
@@ -1,36 +1,28 @@
-// CODE FOR TABS
-
-// const mainTab = document.querySelector(".tablinks.main-tab");
-// const cjTab = document.querySelector(".tablinks.c-j-tab");
-// const iiTab = document.querySelector(".tablinks.i-i-tab");
-// const tvTab = document.querySelector(".tablinks.t-v-tab");
-
-// mainTab.addEventListener('click', openTab("#Main"));
-// cjTab.addEventListener('click', openTab("#China-Japan"));
-// iiTab.addEventListener('click', openTab("#India-Indonesia"));
-// tvTab.addEventListener('click', openTab("#Thai-Vietnam"));
-
-
-// function openTab(kindOfFood) {
-//     console.log(kindOfFood);
-//     let tabcontent = document.querySelectorAll(".tabcontent");
-//     for (let i = 0; i < tabcontent.length; i++) {
-//         tabcontent[i].style.display = "none";
-//         console.log(tabcontent);
-//     }
-
-//     let tablinks = document.querySelectorAll(".tablinks");
-//     for (let i = 0; i < tablinks.length; i++) {
-//         tablinks[i].className = tablinks[i].className.replace(" active", "");
-//         console.log(tablinks);
-//     }
-//     document.querySelector(kindOfFood).style.display = "block";
-//     document.querySelector(kindOfFood).className += " active";
-//     console.log(kindOfFood);
-// }
-
 // CODE FOR EVENTS
-// IDEA: when you click on a "reserve table" button in event section, you go to the form and date will be automatically filled
-// const reserveForm = document.querySelector("#reservation-form");
-// const mainEvent = document.querySelector();
+// event-date, next-event-date
+// IDEA: when you click on a "reserve table" button in event section, 
+//you go to the form and date will be reminded
+const mainEventBtn = document.querySelector("#reserve-event-main");
+const secondEventBtn = document.querySelectorAll(".event-reserve");
+
+const eventDate = document.querySelector(".event-date").innerHTML;
+const nextEventDate = document.querySelectorAll(".next-event-date");
+const date = document.querySelector("#date-label");
+
+
+function changedate(changeDate) {
+    date.innerText = "date of the event is:" + changeDate;
+}
+
+mainEventBtn.addEventListener("click", function () {
+    changedate(eventDate);
+})
+
+
+/*for (i = 0; i > secondEventBtn.length; i++) {
+    secondEventBtn.addEventListener("click", function () {
+        changedate(nextEventDate[i]);
+    });
+} */
+
 
